585644d24c8006328f29734934d49fa891465f4e
AllanJeremy <aj.dev254@gmail.com>
2020-02-17 19:56:37 +0300

    [Partial Feature] Implemented match peers endpoint call

--- src/services/PeerService/matchPeers.js
+++ src/services/PeerService/matchPeers.js
@@ -4,7 +4,11 @@ import {
 
 /** Find matches for the currently logged in peer */
 const matchPeers = async () => {
+    const _requestUrl = `${BASE_API_URL}/peer/match`;
 
+    return fetch(_requestUrl, {
+        method: 'post'
+    });
 }
 
 
