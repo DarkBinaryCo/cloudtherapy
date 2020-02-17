1ac0174579c3453a080a9e05753107cc2cd1addf
AllanJeremy <aj.dev254@gmail.com>
2020-02-17 19:11:06 +0300

    [Enhancement] Added firebase and auth utils

---
+++ src/utils/auth.js
@@ -0,0 +1,52 @@
+import {
+    firebase,
+    auth as firebaseAuth
+} from './firebase';
+
+//* Facebook config
+let facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+//* Google config
+let googleProvider = new firebase.auth.GoogleAuthProvider();
+
+//* Twitter config
+let twitterProvider = new firebase.auth.TwitterAuthProvider();
+
+/** Get the provider to use */
+const _getProvider = (providerName) => {
+    providerName = providerName.toLowerCase();
+    let authProvider = false;
+
+    switch (providerName) {
+        case 'google':
+            authProvider = googleProvider;
+            break;
+        case 'facebook':
+            authProvider = facebookProvider;
+            break;
+        case 'twitter':
+            twitterProvider = twitterProvider;
+            break;
+        default:
+            console.error(`invalid provider "${providerName}" provided. Current options are "facebook", "google and "twitter`);
+    }
+
+    return authProvider;
+}
+
+/** Sign the user in using a redirect */
+const signinWithRedirect = (providerName) => {
+    let authProvider = _getProvider(providerName);
+    firebaseAuth.signInWithRedirect(authProvider);
+}
+
+/** Sign the user in using a popup */
+const signinWithPopup = (providerName) => {
+    let authProvider = _getProvider(providerName);
+    return firebaseAuth.signInWithPopup(authProvider);
+}
+
+export default {
+    signinWithRedirect,
+    signinWithPopup
+}
