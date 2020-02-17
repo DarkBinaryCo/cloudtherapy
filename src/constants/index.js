1df03030dde7b13e3d38853573a8f8e3f756ac14
AllanJeremy <aj.dev254@gmail.com>
2020-02-17 20:57:58 +0300

    Updated CHAT_API_URL to deployed heroku backend

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const SITE_NAME = "Speed Therapy";
 
 export const BASE_API_URL = "https://darklang.com/a/allanjeremy";
-export const CHAT_API_URL = "https://darklang.com/a/allanjeremy";
+export const CHAT_API_URL = "https://cloudtherapy-backend.herokuapp.com";
