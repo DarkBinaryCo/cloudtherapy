a3761f58ac4e30f64d0502d285614eb840706501
AllanJeremy <aj.dev254@gmail.com>
2020-02-18 11:33:42 +0300

    [UI - Enhancement] Added ChatBubble component

---
+++ src/components/ChatBubble.svelte
@@ -0,0 +1,38 @@
+<script>
+  import DateUtils from "../utils/date";
+
+  let positionClass;
+
+  $: positionClass = isSelf ? "right" : "left";
+
+  export const isSelf = true;
+  export const message = {};
+</script>
+
+<style>
+  .chat-bubble {
+    padding: 1rem;
+    min-height: 4rem;
+    max-width: 20rem;
+  }
+
+  .left {
+    float: left;
+    background-color: blue;
+  }
+  .right {
+    float: left;
+    background-color: red;
+  }
+
+  .time {
+    float: right;
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
+</style>
+
+<div class="chat-bubble {positionClass}">
+  <p>{message.text}</p>
+  <span class="time">DateUtils.getTime(message.dateSent.seconds)</span>
+</div>
