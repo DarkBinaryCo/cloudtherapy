c8cd5f3f3aad696c684fc26e6e08b0becbe26ec1
AllanJeremy <aj.dev254@gmail.com>
2020-02-18 07:16:32 +0300

    [UI - Enhancement] Added error page component

---
+++ src/components/ErrorPage.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let error;
+</script>
+
+<style>
+  .error-container {
+    padding: 1rem;
+    border: 1px solid #ff5240;
+    background: rgba(255, 83, 64, 0.25);
+    color: #531b14;
+  }
+</style>
+
+<div class="full-height flex-center-all">
+  <slot name="title" />
+
+  <div class="error-container">
+    <slot>Oops Something went wrong</slot>
+  </div>
+</div>
