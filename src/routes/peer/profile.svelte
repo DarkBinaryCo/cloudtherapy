fc0ddf432c748bfb2d64020d7bacf6b88451cf9f
AllanJeremy <aj.dev254@gmail.com>
2020-02-24 00:19:36 +0300

    [Enhancement] Update profile working

--- src/routes/peer/profile.svelte
+++ src/routes/peer/profile.svelte
@@ -2,31 +2,74 @@
   // Svelte
   import { onMount } from "svelte";
 
+  //
+  import toastr from "toastr";
+
   // Utils
   import { logout } from "../../utils/auth";
 
   // Stores
   import { userStore } from "../../stores";
 
+  // Services
+  import UserService from "../../services/UserService";
+
   // Components
   import Button from "../../components/Button.svelte";
   import TopNav from "../../components/TopNav.svelte";
+  import BottomNav from "../../components/BottomNav.svelte";
 
-  let gender = "male";
   let generatedName = "";
+  let gender = "male";
+  let county;
+
   let profileTitle = "";
   let isNewAccount; // Profile completed or not
 
   let inputGenerateName;
 
+  $: generatedName = $userStore.name;
   $: isNewAccount = $userStore.isNew;
   $: profileTitle = isNewAccount ? "Welcome onboard!" : "Welcome back";
 
   // Generate a random name based on gender
   const generateName = () => {
+    if (!chance) return;
+
     const nameGeneratorOptions = { nationality: "en", gender };
     generatedName = chance.name(nameGeneratorOptions);
     return generatedName;
+  };
+
+  //TODO: Refactor this
+  // Update profile
+  const updateProfile = () => {
+    const _updateData = {
+      name: generatedName,
+      gender,
+      county
+    };
+
+    UserService.updateUser(_updateData)
+      .then(({ data }) => {
+        if (!data.ok)
+          throw new Error("Something went wrong while trying to update user");
+
+        userStore.update(storeVal => {
+          storeVal.isNew = false;
+          return storeVal;
+        });
+
+        // Everything's fine if we got to this point
+        toastr.success("Successfully updated profile", "Sweet!");
+      })
+      .catch(err => {
+        console.error("Update failed: \n", err.message, err);
+        toastr.error(
+          "Failed to update user. If this problem persists, contact support",
+          "Technical error"
+        );
+      });
   };
 
   onMount(() => {
@@ -37,6 +80,7 @@
 <style>
   .container {
     padding: 2rem;
+    padding-top: 0.5rem;
   }
 
   .container h1 {
@@ -96,7 +140,7 @@
   }
 </style>
 
-<TopNav>
+<TopNav hasBack={false}>
   <div slot="right">
     <Button size="sm" id="btn-top-nav" on:click={logout}>Logout</Button>
   </div>
@@ -133,15 +177,19 @@
   </div>
   <div class="input-container">
     <label for="selectCounty">County</label>
-    <select id="selectCounty">
-      <option value="1">Nairobi</option>
+    <select id="selectCounty" bind:value={county}>
+      <option value="nairobi">Nairobi</option>
     </select>
   </div>
 
   <div class="btn-complete-wrapper">
-    <Button id="btn-complete-profile">Complete profile</Button>
+    <Button id="btn-complete-profile" on:click={updateProfile}>
+      Complete profile
+    </Button>
   </div>
 </div>
+
+<BottomNav active="profile" />
 
 <svelte:head>
   <script src="js/chance.min.js">
